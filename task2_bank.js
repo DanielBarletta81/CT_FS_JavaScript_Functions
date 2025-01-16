e132fb84972db9b81afb515d193559c7cbfd70ac
DanielBarletta81 <dbarcelona1481@gmail.com>
2025-01-16 00:43:34 -0500

    add functions for tasks in part 2 task2 bank file

--- task2_bank.js
+++ task2_bank.js
@@ -3,13 +3,28 @@
 //Task 1: Create a function to handle deposits into a bank account.
 //Essentially, adding money to your bank account.
 
-function deposit(){
-    let accountBalance = 0;
-    let depositAmount = 500;
-
+function deposit(depositAmount){
+    let accountBalance = 500;
     let newBalance = accountBalance + depositAmount;
 
-
-    console.log("Balance after deposit:", newBalance);
-
+  return "Balance after deposit" + newBalance
 }
+
+console.log(deposit(1500))
+/* Task 2: Implement a function to handle withdrawals from a bank account. 
+Essentially, taking money out of your bank account. */
+
+
+function withdraw(withdrawalAmount){
+    let accountBalance = 500;
+    let newBalance = accountBalance - withdrawalAmount;
+
+  return "Balance after withdrawal" + newBalance
+}
+
+console.log(withdraw(150))
+
+//Task 3: Develop a function to check the current balance of the account. 
+//Lastly, show much much money you have left.
+
+
