a0575ac16bd86f1e778311cb31fce81416462915
DanielBarletta81 <dbarcelona1481@gmail.com>
2025-01-16 00:16:27 -0500

    add files for task 2

---
+++ task2_bank.js
@@ -0,0 +1,15 @@
+
+
+//Task 1: Create a function to handle deposits into a bank account.
+//Essentially, adding money to your bank account.
+
+function deposit(){
+    let accountBalance = 0;
+    let depositAmount = 500;
+
+    let newBalance = accountBalance + depositAmount;
+
+
+    console.log("Balance after deposit:", newBalance);
+
+}
